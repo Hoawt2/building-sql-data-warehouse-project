9cadbc290fc66f64d5928b6c84abddfe7152f302
howt.hoang24 <hhoangh29@gmail.com>
2025-12-11 15:47:09 +0700

    commit

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -1,8 +1,7 @@
 -- ====================================================================
 -- Checking 'gold.dim_customers'
 -- ====================================================================
--- Check for Uniqueness of Customer Key in gold.dim_customers
--- Expectation: No results 
+-- Expectation: No duplicated surrogate keys
 SELECT 
     customer_key,
     COUNT(*) AS duplicate_count
@@ -11,10 +10,9 @@ GROUP BY customer_key
 HAVING COUNT(*) > 1;
 
 -- ====================================================================
--- Checking 'gold.product_key'
+-- Checking 'gold.dim_products'
 -- ====================================================================
--- Check for Uniqueness of Product Key in gold.dim_products
--- Expectation: No results 
+-- Expectation: No duplicated product surrogate keys
 SELECT 
     product_key,
     COUNT(*) AS duplicate_count
@@ -25,11 +23,15 @@ HAVING COUNT(*) > 1;
 -- ====================================================================
 -- Checking 'gold.fact_sales'
 -- ====================================================================
--- Check the data model connectivity between fact and dimensions
-SELECT * 
+-- Validate fact ↔ dimension relationships
+SELECT 
+    f.*,
+    c.customer_key AS dim_customer_key,
+    p.product_key AS dim_product_key
 FROM gold.fact_sales f
 LEFT JOIN gold.dim_customers c
-ON c.customer_key = f.customer_key
+    ON c.customer_key = f.customer_key
 LEFT JOIN gold.dim_products p
-ON p.product_key = f.product_key
-WHERE p.product_key IS NULL OR c.customer_key IS NULL  
+    ON p.product_key = f.product_key
+WHERE p.product_key IS NULL 
+   OR c.customer_key IS NULL;
